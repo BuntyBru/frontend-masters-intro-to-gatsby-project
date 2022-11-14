b34121c5d1beda700e4f7f9769ad4986f429083f
BuntyBru <buntybru@gmail.com>
2022-11-14 21:15:38 +0530

    create-pages

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const About = () => {
+  return <p>About page</p>;
+};
+
+export default React.memo(About);
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <p>Hello mate</p>
+    </div>
+  );
+};
+
+export default React.memo(IndexPage);
